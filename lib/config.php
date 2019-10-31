761caf59da69f2f0177b9b4d348dc2c4e580a89e
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2019-10-31 19:22:48 +0100

    fixed fixed config.php
    De kell a portszám, mert nélküle nem jó wampon

--- lib/config.php
+++ lib/config.php
@@ -3,7 +3,7 @@
 //        pw:CoolestPasswordEver
 // email küldés local-hoston: https://www.youtube.com/watch?v=qqm64zHlFUg
 define('WSNAME','Nagyon kreatív webshop');
-define('DOMAIN', 'http://localhost/blank_team/');
+define('DOMAIN', 'http://localhost:8080/blank_team/');
 define('LIB_PATH', 'lib/' );       
 
 define('DBH', '127.0.0.1');         
