cde92017e9b4933a83dd44e5ad2a7fc622e6c583
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2019-11-10 18:47:30 +0100

    Visszaírás, css, sok minden

--- products.php
+++ products.php
@@ -2,36 +2,71 @@
 <?php
 $minID = 0;
 $maxID = 12;
+$type;
+$minprice;
+$maxprice;
+$title;
+$publisher;
+$releaseyear;
 $sql = new SQL();
+
 $starterString = "SELECT * FROM `products` WHERE `del` = 0";
+
+if(!is_post()){
+    unset ($_SESSION["platform-pc"]);
+    unset ($_SESSION["platform-xbox360"]);
+    unset ($_SESSION["platform-xboxone"]);
+    unset ($_SESSION["platform-ps2"]);
+    unset ($_SESSION["platform-ps3"]);
+    unset ($_SESSION["platform-ps4"]);
+    unset ($_SESSION["platform-switch"]);
+    unset ($_SESSION["platform-others"]);
+}
     if(is_post())
-    {         
+    {   
+        if(!(isset($_POST['minidtext']) || isset($_POST['maxidtext'])))
+        {   
+            unset ($_SESSION["platform-pc"]);
+            unset ($_SESSION["platform-xbox360"]);
+            unset ($_SESSION["platform-xboxone"]);
+            unset ($_SESSION["platform-ps2"]);
+            unset ($_SESSION["platform-ps3"]);
+            unset ($_SESSION["platform-ps4"]);
+            unset ($_SESSION["platform-switch"]);
+            unset ($_SESSION["platform-others"]);
+        }
         //creating platform list for query
         $platformString = "(";
-        if(isset($_POST['platform-pc']) && $_POST['platform-pc'] == "PC") $platformString .="'PC',";
-        if(isset($_POST['platform-xbox360']) && $_POST['platform-xbox360'] == "XBOX 360") $platformString .="'XBOX 360',";
-        if(isset($_POST['platform-xboxone']) && $_POST['platform-xboxone'] == "XBOX One") $platformString .="'XBOX One',";
-        if(isset($_POST['platform-ps2']) && $_POST['platform-ps2'] == "PS2") $platformString .="'PS2',";
-        if(isset($_POST['platform-ps3']) && $_POST['platform-ps3'] == "PS3") $platformString .="'PS3',";
-        if(isset($_POST['platform-ps4']) && $_POST['platform-ps4'] == "PS4") $platformString .="'PS4',";
-        if(isset($_POST['platform-switch']) && $_POST['platform-switch'] == "Nintendo Switch") $platformString .="'Nintendo Switch',";
+        if(isset($_POST['platform-pc']) && $_POST['platform-pc'] == "PC") {$platformString .="'PC',"; $_SESSION['platform-pc'] = $_POST['platform-pc'];}
+        if(isset($_POST['platform-xbox360']) && $_POST['platform-xbox360'] == "XBOX 360") {$platformString .="'XBOX 360',"; $_SESSION['platform-xbox360'] = $_POST['platform-xbox360'];}
+        if(isset($_POST['platform-xboxone']) && $_POST['platform-xboxone'] == "XBOX One") {$platformString .="'XBOX One',"; $_SESSION['platform-xboxone'] = $_POST['platform-xboxone'];}
+        if(isset($_POST['platform-ps2']) && $_POST['platform-ps2'] == "PS2") {$platformString .="'PS2',"; $_SESSION['platform-ps2'] = $_POST['platform-ps2'];}
+        if(isset($_POST['platform-ps3']) && $_POST['platform-ps3'] == "PS3") {$platformString .="'PS3',"; $_SESSION['platform-ps3'] = $_POST['platform-ps3'];}
+        if(isset($_POST['platform-ps4']) && $_POST['platform-ps4'] == "PS4") {$platformString .="'PS4',"; $_SESSION['platform-ps4'] = $_POST['platform-ps4'];}
+        if(isset($_POST['platform-switch']) && $_POST['platform-switch'] == "Nintendo Switch") {$platformString .="'Nintendo Switch',"; $_SESSION['platform-switch'] = $_POST['platform-switch'];}
+        if(isset($_POST['platform-others']) && $_POST['platform-others'] == "Others") $_SESSION['platform-others'] = $_POST['platform-others'];
         $platformString = rtrim(trim($platformString), ',');
         $platformString .=")";   
 
         //setting values for filters
-        if(isset($_POST['slidermin']))$minprice = $_POST['slidermin'];
-        if(isset($_POST['slidermax']))$maxprice = $_POST['slidermax'];
-
-       
-        if(isset($_POST['title']))$title = $_POST['title'];
+        if(isset($_POST['slidermin'])) $minprice = $_POST['slidermin'];
+        if(isset($_POST['slidermax'])) $maxprice = $_POST['slidermax'];
+        if(isset($_POST['title'])) $title = $_POST['title'];
         if(isset($_POST['type']) && $_POST['type'] != "Select Type") $type = $_POST['type'];
-        if(isset($_POST['release-year']))$releaseyear = $_POST['release-year'];
-        if(isset($_POST['publisher']))$publisher = $_POST['publisher'];
+        if(isset($_POST['release-year'])) $releaseyear = $_POST['release-year'];
+        if(isset($_POST['publisher'])) $publisher = $_POST['publisher'];
     } 
         
         //Filter search
         if(!(isset($_POST['minidtext']) || isset($_POST['maxidtext'])))
         { 
+            
+            if(isset($minprice))$_SESSION['minprice'] = $minprice; 
+            if(isset($maxprice))$_SESSION['maxprice'] = $maxprice; 
+            if(isset($title))$_SESSION['title'] = $title;
+            if(isset($type))$_SESSION['type'] = $type; 
+            if(isset($releaseyear))$_SESSION['releaseyear'] = $releaseyear; 
+            if(isset($publisher))$_SESSION['publisher'] = $publisher;  
             $sql_string = isset($_POST['slidermin']) || isset($_POST['slidermax']) ? " AND `price` BETWEEN $minprice AND $maxprice" : ""; 
             $sql_string .= isset($_POST['title']) && $_POST['title'] != null ? " AND `title` LIKE '%$title%'" : ""; 
             $sql_string .= isset($_POST['type']) && $_POST['type'] != "Select Type" ? " AND `type` ='$type'" : "";  
@@ -52,16 +87,25 @@ $starterString = "SELECT * FROM `products` WHERE `del` = 0";
                
             $_SESSION['sql_query'] = $sql_string;
             $product = $sql->execute($starterString.$sql_string." LIMIT $minID,$maxID"); 
-            echo $starterString.$sql_string." LIMIT $minID,$maxID";
+            //echo $starterString.$sql_string." LIMIT $minID,$maxID"; //<--- QUERY
 
         //changing pages via buttons  
         } else {  
+            if(isset($_SESSION['minprice']))$minprice = $_SESSION['minprice'];
+            if(isset($_SESSION['maxprice']))$maxprice = $_SESSION['maxprice'];
+            if(isset($_SESSION['title']))$title = $_SESSION['title'];
+            if(isset($_SESSION['type']))$type = $_SESSION['type'];
+            if(isset($_SESSION['releaseyear']))$releaseyear = $_SESSION['releaseyear'];
+            if(isset($_SESSION['publisher']))$publisher = $_SESSION['publisher']; 
+
             if(isset($_POST['minidtext'])) $minID = $_POST['minidtext'];
             if(isset($_POST['increase']))$minID += 12;
             else if(isset($_POST['decrease']) && $minID >1) $minID -= 12;
 
             $sql_string = $_SESSION['sql_query'];
             $product = $sql->execute($starterString.$sql_string." LIMIT $minID,$maxID");
+
+           
         }
 
     $sql_string2 = "SELECT COUNT(*) AS `records` FROM `products` WHERE `del` = 0";
@@ -83,15 +127,15 @@ $starterString = "SELECT * FROM `products` WHERE `del` = 0";
                 range: true,
                 min: 0,
                 max: 200,
-                values: [ 0, 200 ],
+                values: [0, 200],
                 slide: function( event, ui ) {
                         document.getElementById('slidermin').value = ui.values[0];
                         document.getElementById('slidermax').value = ui.values[1];
-                    $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+                    $( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
                 }
                 });
-                $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-                " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+                $( "#amount" ).val( "€" + $( "#slider-range" ).slider( "values", 0 ) +
+                " - €" + $( "#slider-range" ).slider( "values", 1 ) );
             } );
             </script> 
             <p>
@@ -102,39 +146,42 @@ $starterString = "SELECT * FROM `products` WHERE `del` = 0";
         </div>
         <div class ="filter-title">
             <label for="title">Title</label>
-            <input type="text" name ="title">
+            <input type="text" name ="title" <?php if(isset($title) && $title != null) echo "value=$title"; ?>>
         </div>
         <div class="filter-platform">
             <p>Platforms</p>
-            <input type="checkbox" name="platform-pc" value="PC">PC<br>
-            <input type="checkbox" name="platform-xbox360" value="XBOX 360">XBOX 360<br>
-            <input type="checkbox" name="platform-xboxone" value="XBOX One">XBOX One<br>
-            <input type="checkbox" name="platform-ps2" value="PS2">PS2<br>
-            <input type="checkbox" name="platform-ps3" value="PS3">PS3<br>
-            <input type="checkbox" name="platform-ps4" value="PS4">PS4<br>
-            <input type="checkbox" name="platform-switch" value="Nintendo Switch">Nintendo Switch<br>
-            <input type="checkbox" name="platform-others" value="Others">Others<br>
+            <input type="checkbox" name="platform-pc" value="PC" <?php if(isset($_SESSION['platform-pc'])) echo "checked"; ?>>PC<br>
+            <input type="checkbox" name="platform-xbox360" value="XBOX 360" <?php if(isset($_SESSION['platform-xbox360'])) echo "checked"; ?>>XBOX 360<br>
+            <input type="checkbox" name="platform-xboxone" value="XBOX One" <?php if(isset($_SESSION['platform-xboxone'])) echo "checked"; ?>>XBOX One<br>
+            <input type="checkbox" name="platform-ps2" value="PS2" <?php if(isset($_SESSION['platform-ps2'])) echo "checked"; ?>>PS2<br>
+            <input type="checkbox" name="platform-ps3" value="PS3" <?php if(isset($_SESSION['platform-ps3'])) echo "checked"; ?>>PS3<br>
+            <input type="checkbox" name="platform-ps4" value="PS4" <?php if(isset($_SESSION['platform-ps4'])) echo "checked"; ?>>PS4<br>
+            <input type="checkbox" name="platform-switch" value="Nintendo Switch" <?php if(isset($_SESSION['platform-switch'])) echo "checked"; ?>>Nintendo Switch<br>
+            <input type="checkbox" name="platform-others" value="Others" <?php if(isset($_SESSION['platform-others'])) echo "checked"; ?>>Others<br>
         </div>
         
         <div class="filter-type">
             <p><label for="type"> Type </label></p> <br>
             <select name ="type">
                 <option value="Select Type" selected>Select Type</option>
-                <option value="Strategy">Strategy</option>
-                <option value="Shooter">Shooter</option>
-                <option value="Moba">Moba</option>
-                <option value="Fighter">Fighter</option>
-                <option value="Sport">Sport</option>
-                <option value="Open world">Open world</option>
+                <option value="Strategy"  <?php if(isset($_POST['type']) && $_POST['type'] == "Strategy") echo "selected"; ?>>Strategy</option>
+                <option value="Shooter" <?php if(isset($_POST['type']) && $_POST['type'] == "Shooter") echo "selected"; ?>>Shooter</option>
+                <option value="Moba" <?php if(isset($_POST['type']) && $_POST['type'] == "Moba") echo "selected"; ?>>Moba</option>
+                <option value="Fighter" <?php if(isset($_POST['type']) && $_POST['type'] == "Fighter") echo "selected"; ?>>Fighter</option>
+                <option value="Sport" <?php if(isset($_POST['type']) && $_POST['type'] == "Sport") echo "selected"; ?>>Sport</option>
+                <option value="Open world" <?php if(isset($_POST['type']) && $_POST['type'] == "Open world") echo "selected"; ?>>Open world</option>
+                <option value="Battle Royale" <?php if(isset($_POST['type']) && $_POST['type'] == "Battle Royale") echo "selected"; ?>>Battle Royale</option>
+                <option value="MMO" <?php if(isset($_POST['type']) && $_POST['type'] == "MMO") echo "selected"; ?>>MMO</option>
+                <option value="RPG" <?php if(isset($_POST['type']) && $_POST['type'] == "RPG") echo "selected"; ?>>RPG</option>
             </select> <br>
         </div>
         <div class="filter-release">
             <p><label>Release year</label></p><br>
-            <input name="release-year" type="number" min="1990" max="<?php echo date('Y') ?>">
+            <input name="release-year" type="number" min="1990" max="<?php echo date('Y') ?>" <?php if(isset($releaseyear) && $releaseyear != null) echo "value=$releaseyear"; ?>>
         </div>
         <div class="filter-publisher">
             <p><label>Publisher</label></p>
-            <input name="publisher" type="text-p">
+            <input name="publisher" type="text-p" <?php if(isset($publisher) && $publisher != null) echo "value=$publisher"; ?>>
         </div>
         <div class="filter-btn-div"><button class="filter-btn" type="submit-btn"><span>Search</span></button></div>
         <input type="text" name ="slidermin" id="slidermin" value = 0 style = "display: none">
