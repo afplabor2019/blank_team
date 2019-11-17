9d1f01e8fc0b48ac650ef87545e9fe2216af78f8
noemitth10 <noemitth10@users.noreply.github.com>
2019-11-17 20:58:55 +0100

    Header

--- pages/head.php
+++ pages/head.php
@@ -24,80 +24,56 @@
     header("Location: ".url('products')."&n=$name");
   }
 
-?>
-    <header>
-    <div class="header">
-                <div class="upper-row">
-                    <nav>
-                        <ul>
-                            <li>
-                                <a class="mainlogo" href ="<?php echo url('home') ?>">GamerZ</a>
-                            </li>
-                            <li>
-                                <div class="search-container">
-                                  <form action="" method="POST">
-                                  <input class ="searchbar" id="searchtext" type="text" placeholder ="Search..." name="search">
-                                  <button type="submit" id="search" ><img class="icon" src="images/search.png" alt="Search" height="17" width="17"></button>
-                                  </form>
-                                </div>
-                            </li>
-                                              
-                                  <div class="popup" onclick="myFunction2()" style="display:inline;margin:0;padding:0;">
-                                  <?php if(!loggedIn()) : ?>
-                                    <span class="popuptext" id="myPopup">
-                                    <a href="<?php echo url('login') ?>"> Login</a><br>
-                                    <a href="<?php echo url('register') ?>"> Register</a>
-                                    </span>
-                                  <?php else : ?>
-                                  <span class="popuptext" id="myPopup">
-                                  <a href="<?php echo url('profile') ?>"> Profile</a><br>
-                                    <a href="<?php echo isset($_GET['p']) ? $_SERVER['REQUEST_URI']."&e=1": $_SERVER['REQUEST_URI']."?p=home&e=1" ;?>"> Log out</a>
-                                    </span>
-                                  <?php endif; ?>
-                                  <img src="<?php echo isset($_SESSION['user_id']) ? $profilepic[0]['profile_pic'] : $profilepic ?>" class ="login-img" alt="Profile">
-                              </div>
-                              <a href ="<?php echo url('shoppingcart') ?>"><img src="images/cart2.png" alt="Shopping cart" class="shoppingcart-img"></a>
-                           
-                        </ul> 
-                    </nav>
-                </div>
-            <div class="topnav" id="myTopnav">
-              <a href="<?php echo url('home') ?>" <?php echo $page == 'home' ? 'class="active"' : ''; ?>>Home</a>
-              <a href="<?php echo url('products') ?>" <?php echo $page == 'products' ? 'class="active"' : ''; ?>>Products</a>
-              <div class="dropdown">
-                <button class="dropbtn">Platform 
-                  <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                  <a href="<?php echo url('products')."&platform=PC" ?>" >PC</a>
-                  <a href="<?php echo url('products')."&platform=XBOX One" ?>">XBOX ONE</a>
-                  <a href="<?php echo url('products')."&platform=PS4" ?>">PS4</a>
-                  <a href="<?php echo url('products')."&platform=Nintendo Switch" ?>">Nintendo Switch</a>
-                </div>
-              </div>
-              <a href="<?php echo url('contact') ?>" <?php echo $page == 'contact' ? 'class="active"' : ''; ?>>Contact</a>
-              <?php if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 1) :?>
-              <a href="<?php echo url('addProduct') ?>" <?php echo $page == 'addProduct' ? 'class="active"' : '';?>>Add Product</a>
-                <?php endif;?>
-              <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-              
-            </div>
-<script>
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+?>   
+<header class="header">
+  <div class="upper-row">
+    <div class="upper-row-content">
+      <a class="mainlogo" href ="<?php echo url('home') ?>">GamerZ</a>
+      <div class="wrap">
+        <div class="search">
+            <input type="text" class="searchTerm" placeholder="What are you looking for?">
+            <button type="submit" class="searchButton">
+              <i class="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="topnav" id="myTopnav">
+                      <a href="<?php echo url('home') ?>" <?php echo $page == 'home' ? 'class="active"' : ''; ?>>Home</a>
+                      <a href="<?php echo url('products') ?>" <?php echo $page == 'products' ? 'class="active"' : ''; ?>>Products</a>
+                      <div class="dropdown">
+                        <button class="dropbtn">Platform 
+                          <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                          <a href="<?php echo url('products')."&platform=PC" ?>" >PC</a>
+                          <a href="<?php echo url('products')."&platform=XBOX One" ?>">XBOX ONE</a>
+                          <a href="<?php echo url('products')."&platform=PS4" ?>">PS4</a>
+                          <a href="<?php echo url('products')."&platform=Nintendo Switch" ?>">Nintendo Switch</a>
+                        </div>
+                      </div>
+                      <a href="<?php echo url('contact') ?>" <?php echo $page == 'contact' ? 'class="active"' : ''; ?>>Contact</a>
+                      <?php if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 1) :?>
+                      <a href="<?php echo url('addProduct') ?>" <?php echo $page == 'addProduct' ? 'class="active"' : '';?>>Add Product</a>
+                        <?php endif;?>
+                      <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>  
+    </div>
+                    <script>
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
     }
-  }
 
-  function myFunction2() {
-    var popup = document.getElementById("myPopup");
-   popup.classList.toggle("show");
-  }
-</script>
+    function myFunction2() {
+      var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    }
+  </script>
 
-</div>
-    </header>
+</header>
 <main class="container">
