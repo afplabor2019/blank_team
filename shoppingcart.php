2f89f804b092043c0afc6c6e7e028a95031e57df
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2019-11-21 20:43:02 +0100

    Shopping cart improvements

--- shoppingcart.php
+++ shoppingcart.php
@@ -13,33 +13,42 @@ $orders_array = rtrim(trim($orders_array), ',');
 $orders_array.= ")";
 
 //getting the order_item of the user using order_ids
+if($orders_array != '()')
 $order_item = $sql->execute("SELECT * FROM `order_item` WHERE `order_id` IN $orders_array");
 
 //displaying orders
 ?>
-<div class ="shopping-cart-container">
-<h1 class="login-h">SHOPPING CART</h1>  
-<div class="cart-border">
-<?php
-foreach ($order_item as $key => $value) : ?>
-<?php 
-    $item = $sql->execute("SELECT * FROM `products` WHERE id = ?",$value['item_id']);
-    $row += 1;
 
-?> 
-    <div class ="shopping-cart-row" style="background-color:<?php echo $row % 2 == 0 ? 'white' : '' ?>">
-        <span class ="shopping-cart-delete-cross"> &#10060</span>
-        <span class ="shopping-cart-gametitle" title="<?php echo $item[0]['title'] ?>"><?php echo $item[0]['title'] ?></span>
-        <span class ="shopping-cart-platform"><?php echo $item[0]['platform'] ?></span>
-        <span class ="shopping-cart-unitprice">Unit price : <?php echo $item[0]['price'] ?> €</span>
-        <span  class ="shopping-cart-quantity-text">quantity:</span>
-        <input class="shopping-cart-quantity" type="number" name ="quantity" id = "quantity" value="1">
-        <span class ="shopping-cart-total">Total : <?php echo $item[0]['price']?></span><br>
-        
-    </div>
-<?php endforeach; ?>
+<?php
+//if there are items in the shopping cart
+if($orders_array != '()'){
+    ?><div class ="shopping-cart-container">
+    <h1 class="login-h">SHOPPING CART</h1>  
+    <div class="cart-border">
+    <?php foreach ($order_item as $key => $value) : ?>
+    <?php 
+        $item = $sql->execute("SELECT * FROM `products` WHERE id = ?",$value['item_id']);
+        $row += 1;  
+    ?> 
+        <div class ="shopping-cart-row" style="background-color:<?php echo $row % 2 == 0 ? 'white' : '' ?>">
+            <span class ="shopping-cart-delete-cross"> &#10060</span>
+            <span class ="shopping-cart-gametitle" title="<?php echo $item[0]['title'] ?>"><?php echo $item[0]['title'] ?></span>
+            <span class ="shopping-cart-platform"><?php echo $item[0]['platform'] ?></span>
+            <span class ="shopping-cart-unitprice" title="Unit price">U.p. : <?php echo $item[0]['price'] ?> €</span>
+            <span  class ="shopping-cart-quantity-text">quantity:</span>
+            <input class="shopping-cart-quantity" type="number" name ="quantity" id = "quantity" value="1">
+            <span class ="shopping-cart-total">Total : <?php echo $item[0]['price']?></span><br>
+        </div>
+    <?php endforeach; ?>
+
+
 </div>
     <span class ="sp-total">Total :</span><br>
     <button class ="shopping-cart-btn" type="submit"><Span>Buy</span></button>
 </div>
+<?php }
+//if there are no items in the shopping cart.
+else{
+
+} ?>
 <?php require_once "pages/footer.php"; ?>
