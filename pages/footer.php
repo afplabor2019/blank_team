77ebfc455dc1df434e336b8cb370585d4f27557a
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2019-11-21 18:55:06 +0100

    Footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,13 +1,31 @@
 </main>
 <footer>
     <div class="footer">
+        <div class ="footer-left-side">
         <p>&copy; 2019. Blank Team. All rights reserved. </p>
-        <div>
-            </p>Vásárlási információk</p>
+            <ul>
+                <li>
+                    <p>2005-2019 Mozilla and individual contributors. Content is available under 
+                    <a target="blank" href="https://developer.mozilla.org/hu/docs/MDN/About#Copyrights_and_licenses">this link</a>
+                    </p>
+                </li>
+                <li>
+                    <a target="blank" href="https://www.webopedia.com/TERM/F/FAQ.html"><p>Frequently asked questions(FAQ)</p></a>
+                </li>
+                <li>
+                    <a target="blank"href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/">Terms and Conditions</a>
+                </li>
+                <li>
+                    <p>Any question? Contact <a href="<?php echo url('contact') ?>">coolestwebshop@gmail.com</a></p>
+                </li>
+                
+            </ul>
+               
         </div>
-        <div>
-            </p>Kérdésed van</p>
-            </p>Email cím</p>
+        <div class ="footer-right-side">
+            <a href="https://www.facebook.com/" target="blank"><img src="images/fblogo.png" alt="Facebook logo"></a>
+            <a href="https://www.instagram.com/?hl=hu" target="blank"><img src="images/instalogo.png" alt="Instagram logo"></a>
+            <a href="https://twitter.com/?lang=hu" target="blank"><img src="images/twitter logo.png" alt="Twitter logo"></a>
         </div>
     </div>
 </footer>
