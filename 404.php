8b68b2488075dbe4f63d3497185ecc8f15e2d44c
noemitth.10 <noemitth.10@gmail.com>
2019-11-21 22:59:23 +0100

    404

--- 404.php
+++ 404.php
@@ -1,1 +1,4 @@
-404
+<?php include_once "pages/head.php";?>
+<img src="images/404.png" alt="404 - Page not found" height="auto" width="80%" style="margin:0 10%">
+<p style="text-align: center; font-size:24px">Oops... The page, that you searched for is not exists.</p>
+<?php include_once "pages/footer.php";?>
