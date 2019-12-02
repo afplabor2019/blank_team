484253b4257b5ec0d74f02964b48aee2a7d5d9bf
noemitth10 <noemitth.10@gmail.com>
2019-12-02 14:36:22 +0100

    Opinion-box fix

--- home.php
+++ home.php
@@ -68,48 +68,6 @@ foreach ($best_scored as $key => $value) {
         </div>
         </section>
         <div></div>
-        <!--<div class="home-title">                                    NE NYÚLJ HOZZÁ
-            <h1 class="home-titles">Noémi 100x jobb munkája</h1> 
-            <div class="w3-content w3-display-container" style="max-width:800px">
-            <img class="mySlides" src="images/adimages/dscover.jpg" style="width:100%">
-            <img class="mySlides" src="images/adimages/witchercover.jpg" style="width:100%">
-            <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
-            <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
-            <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
-            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
-            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-            </div>
-            </div>
-
-            <script>
-            var slideIndex = 1;
-            showDivs(slideIndex);
-
-            function plusDivs(n) {
-            showDivs(slideIndex += n);
-            }
-
-            function currentDiv(n) {
-            showDivs(slideIndex = n);
-            }
-
-            function showDivs(n) {
-            var i;
-            var x = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("demo");
-            if (n > x.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = x.length}
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";  
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" w3-white", "");
-            }
-            x[slideIndex-1].style.display = "block";  
-            dots[slideIndex-1].className += " w3-white";
-            }
-            </script>
-        </div>-->
         <div class="home-title">
             <h1 class="home-titles">BEST REVIEWED</h1>  
             <p>A legjobban értékelt játékokból 5-6 megjelenitése egymás mellett és két irányba lapozása</p>
@@ -137,7 +95,7 @@ foreach ($best_scored as $key => $value) {
             $message = $value['msg'];
             $score = $value['score'];
             $generatedid=GenerateID(4);    
-            echo "<div class='h-opinion-left-side'><img src=".$profilepic[0]['profile_pic']." class=p-review-profile-pic>";
+            echo "<div class='h-o-box-peruser'><div class='h-opinion-left-side'><img src=".$profilepic[0]['profile_pic']." class=p-review-profile-pic>";
             echo "<p>".$username[0]['user_name']."</p></div>";
             echo "<span class=rating>";
             if($score == 5) echo "<input type=radio class=rating-input id=rating-input-1-5 name=rating-input$generatedid value =5 checked>";
@@ -156,7 +114,7 @@ foreach ($best_scored as $key => $value) {
             else echo "<input type=radio class=rating-input id=rating-input-1-1 name=rating-input$generatedid value =1>";
             echo "<label for=rating-input-1-1 class=rating-star></label>";
             echo "</span><br><br><br>";
-            echo "<div class='message-box'><p>$message</p></div>";  
+            echo "<div class='message-box'><p>$message</p></div></div>";  
         }
         ?>
         </div>
