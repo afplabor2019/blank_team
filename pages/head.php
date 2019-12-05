1e2978b1c05bceadf65689c900aeb93a67fb5f86
dombidav <40638464+dombidav@users.noreply.github.com>
2019-12-05 19:30:47 +0100

    Update head.css

--- pages/head.php
+++ pages/head.php
@@ -9,10 +9,10 @@
     <title>GamerZ</title>
     <!--link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"-->
     
-    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <!--link rel="stylesheet" href="style/bootstrap.min.css">
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script-->
+
     <link rel="stylesheet" href="style/app.css">
     <link rel="stylesheet" href="style/footer.css">
     <link rel="stylesheet" href="style/login.css">
