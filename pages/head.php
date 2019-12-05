8db0395938e77cabbcf5e83daffe5dd9636f2013
dombidav <40638464+dombidav@users.noreply.github.com>
2019-12-05 19:32:13 +0100

    Just another css fix

--- pages/head.php
+++ pages/head.php
@@ -9,9 +9,9 @@
     <title>GamerZ</title>
     <!--link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"-->
     
-    <!--link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script-->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="style/app.css">
     <link rel="stylesheet" href="style/footer.css">
